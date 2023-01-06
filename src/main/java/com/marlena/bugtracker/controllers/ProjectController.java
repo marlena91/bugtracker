5e0d482c665d7fae821f207748bf39eb74945c45
Marlena Zarczynska <marlena.zarczynska@gmail.com>
2023-01-06 21:18:12 +0100

    Created newProject.html with form

--- src/main/java/com/marlena/bugtracker/controllers/ProjectController.java
+++ src/main/java/com/marlena/bugtracker/controllers/ProjectController.java
@@ -4,6 +4,7 @@ import com.marlena.bugtracker.models.Project;
 import com.marlena.bugtracker.services.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,11 +19,21 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @GetMapping
-    ModelAndView index() {
+    public ModelAndView index() {
         List<Project> projects = projectService.findAll();
         ModelAndView modelAndView = new ModelAndView("projects/index");
         modelAndView.addObject("projects", projects);
 
         return modelAndView;
     }
+
+    @GetMapping("/new")
+    public String displayNewProjectForm(Model model) {
+        model.addAttribute("project", new Project());
+        return "projects/newProject.html";
+    }
+
+
+
+
 }
